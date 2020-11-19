edba515f1e4e0d01bdddde5a3a3092d6667d998a
ACupofAir <timethoughts@outlook.com>
2020-11-19 09:30:21 +0800

    Half ch5 exp

--- DataStructure/chp5_Tree/experiment/Tree.cpp
+++ DataStructure/chp5_Tree/experiment/Tree.cpp
@@ -1,16 +1,37 @@
 #include "Tree.h"
-                                 
-int main(void) {                 
-  Tree<int> test_tree;            
-  TreeNode<int> a(1);             
-  TreeNode<int> b(2);             
-  TreeNode<int> c(3);             
-  TreeNode<int> d(4);             
-  test_tree.root = &a;            
-  test_tree.root->leftChild = &b; 
-  test_tree.root->rightChild = &c;
-  c.leftChild = &d;               
-  test_tree.Inorder();            
-  return 0;                       
-}                                 
                                   
+int main(void) {                  
+  Tree<int> test_tree;            
+  TreeNode<int> a(1);              
+  TreeNode<int> b(2);              
+  TreeNode<int> c(3);              
+  TreeNode<int> d(4);              
+  TreeNode<int> e(5);              
+  TreeNode<int> f(6);              
+  TreeNode<int> g(7);              
+  test_tree.root = &a;             
+  a.leftChild = &b;                
+  a.rightChild = &c;               
+  b.leftChild = &d;                
+  b.rightChild = &e;               
+  c.leftChild = &f;                
+  c.rightChild = &g;               
+  d.leftChild = NULL;              
+  d.rightChild = NULL;             
+  e.leftChild = NULL;               
+  e.rightChild = NULL;              
+  f.leftChild = NULL;               
+  f.rightChild = NULL;              
+  g.leftChild = NULL;               
+  g.rightChild = NULL;              
+  cout<<"Inorder: ";                
+  test_tree.Inorder();              
+  cout<<"\nPreorder:";              
+  test_tree.Preorder();             
+  cout<<"\nPostorder:";             
+  test_tree.Postorder();            
+  cout<<"\nLevelorder:";             
+  test_tree.LevelOrder();            
+  return 0;                         
+}                                   
+                                    
--- DataStructure/chp5_Tree/experiment/Tree.h
+++ DataStructure/chp5_Tree/experiment/Tree.h
@@ -1,7 +1,8 @@
 #ifndef TREE_H
 #define TREE_H                
-#include <iostream>
-using namespace std;   
+#include <iostream>           
+#include <queue>
+using namespace std;          
                                
                                
 template <class T> class Tree; 
@@ -10,42 +11,90 @@ template <class T>
 class TreeNode                 
 {                              
   friend class Tree<T>;        
-  public:               
-    TreeNode(T data) {  
-      this->data = data;
-    }                   
   public:                     
-    T data;                    
-    TreeNode<T> *leftChild;          
-    TreeNode<T> *rightChild;         
-};                                   
-                                     
-template <class T>                   
+    TreeNode(T data) {        
+      this->data = data;      
+    }                                        
+  public:                                    
+    T data;                                  
+    TreeNode<T> *leftChild;                  
+    TreeNode<T> *rightChild;                 
+};                                           
+                                             
+template <class T>                           
 class Tree                                    
 {                                             
   public:                                     
+    TreeNode<T> *root;                        
   //operations                                
     void Inorder();                           
     void Inorder(TreeNode<T>*);               
-                                              
-                                              
-  // private:                                    
-    TreeNode<T> *root;                        
-                                              
+    void Preorder();                         
+    void Preorder(TreeNode<T>*);             
+    void Postorder();                         
+    void Postorder(TreeNode<T>*);             
+    void LevelOrder();
 };                                            
                                               
 template <class T>                            
 void Tree<T>::Inorder(){                      
   Inorder(root);                              
-};                                            
-                                              
-template <class T>                            
-void Tree<T>::Inorder(TreeNode<T> *currentNode){
-  if(currentNode) {                           
-    Inorder(currentNode->leftChild);          
-    cout<<currentNode->data<<"->";            
-    Inorder(currentNode->rightChild);         
-  }                                           
-};                                            
-                                              
-#endif                                        
+}                                            
+                                                     
+template <class T>                                   
+void Tree<T>::Inorder(TreeNode<T> *currentNode){     
+  if(currentNode) {                                  
+    Inorder(currentNode->leftChild);                 
+    cout<<currentNode->data<<"->";                   
+    Inorder(currentNode->rightChild);                                
+  }                                                                  
+}                                                                    
+                                                                     
+template <class T>                                                   
+void Tree<T>::Preorder() {                                           
+  Preorder(root);                                                    
+}                                                                    
+                                                                     
+template <class T>                                                   
+void Tree<T>::Preorder(TreeNode<T>* currentNode) {                   
+  if(currentNode) {                                                  
+    cout<<currentNode->data<<"->";                                   
+    Preorder(currentNode->leftChild);                                
+    Preorder(currentNode->rightChild);                               
+  }                                                                  
+}                                                                    
+                                                                     
+template <class T>                                                   
+void Tree<T>::Postorder() {                                           
+  Postorder(root);                                                    
+}                                                                    
+                                                                     
+template <class T>                                                   
+void Tree<T>::Postorder(TreeNode<T>* currentNode) {                   
+  if(currentNode) {                                                  
+    Postorder(currentNode->leftChild);                                
+    Postorder(currentNode->rightChild);                               
+    cout<<currentNode->data<<"->";                                   
+  }                                                                  
+}                                                                    
+                                                              
+template <class T>                                                   
+void Tree<T>::LevelOrder() {                                  
+  queue<TreeNode<T>*> q;                                      
+  TreeNode<T>* currentNode = root;                            
+  while(currentNode) {                                        
+    cout<<currentNode->data<<"->";                                  
+    if(currentNode->leftChild) q.push(currentNode->leftChild);
+    if(currentNode->rightChild) q.push(currentNode->rightChild);
+    if (q.empty())         
+    {                      
+      return;              
+    }                      
+    else                   
+    {                      
+      currentNode = q.front();
+      q.pop();             
+    }                      
+  }                                                           
+}                                                             
+#endif                                                               
