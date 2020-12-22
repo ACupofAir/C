a5f44da53f3b6303d59e75ee8cb08f95475d0361
ACupofAir <timethoughts@outlook.com>
2020-12-22 19:13:14 +0800

    Fix the rule of comparation of pair

--- DataStructure/chp7_Sort/Homework/Sort_405_5.cpp
+++ DataStructure/chp7_Sort/Homework/Sort_405_5.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-#include <utility>
 #include "QuickSort.h"
+#include "Pair.h"
 
 using namespace std;
 
 int main(void)
 {
-  pair<int, int> arr[3];
-  arr[0] = pair<int,int>(2,3);
-  arr[1] = pair<int,int>(2,1);
-  arr[2] = pair<int,int>(1,6);
-  arr[3] = pair<int,int>(3,7);
+  Pair<int, int> arr[4];
+  arr[0] = Pair<int,int>(1,3);
+  arr[1] = Pair<int,int>(2,1);
+  arr[2] = Pair<int,int>(1,6);
+  arr[3] = Pair<int,int>(3,7);
   quickSort(arr, 0, 3);
   for(int i = 0; i < 4; i++)
   {
-    cout<<"<"<<arr[i].first<<","<<arr[i].second<<">"<<endl;
+    cout<<"<"<<arr[i].first()<<","<<arr[i].second()<<">"<<endl;
   }
   return 0;
 }
--- DataStructure/chp7_Sort/Homework/Sort_416_2.cpp
+++ DataStructure/chp7_Sort/Homework/Sort_416_2.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include "HeapSort.h"
+#include "Pair.h"
 
 using namespace std;
 
 int main(void)
 {
-  pair<int, int> arr[7];
-  arr[1] = pair<int,int>(2,1);
-  arr[2] = pair<int,int>(1,2);
-  arr[3] = pair<int,int>(3,6);
-  arr[4] = pair<int,int>(3,3);
-  arr[5] = pair<int,int>(3,4);
-  arr[6] = pair<int,int>(6,5);
+  Pair<int, int> arr[7];
+  arr[1] = Pair<int,int>(2,1);
+  arr[2] = Pair<int,int>(1,2);
+  arr[3] = Pair<int,int>(3,6);
+  arr[4] = Pair<int,int>(3,3);
+  arr[5] = Pair<int,int>(3,4);
+  arr[6] = Pair<int,int>(6,5);
   HeapSort(arr, 6);
   for(int i = 1; i <= 6; i++)
   {
-    cout<<"<"<<arr[i].first<<","<<arr[i].second<<">"<<endl;
+    cout<<"<"<<arr[i].first()<<","<<arr[i].second()<<">"<<endl;
   }
   return 0;
 }
